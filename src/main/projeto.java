b3caa9440b8c5e55aaa9b86a3655cb1c4f239d63
RomeroGabriel <gabrielromerods@hotmail.com>
2019-01-14 20:00:46 -0200

    Testando

--- src/main/projeto.java
+++ src/main/projeto.java
@@ -15,21 +15,20 @@ import org.openqa.selenium.interactions.Actions;
 public class projeto {
 	
 	public static void main(String[] args) throws IOException, InterruptedException{
-		System.setProperty("webdriver.chrome.driver", "/home/romero/Documentos/Faculdade/IC/Drivers-Selenium/Navegadores/chromedriver");
+		System.setProperty("webdriver.chrome.driver", "/home/romero/Documentos/IC/Drivers-Selenium/Navegadores/chromedriver");
 		
 		WebDriver site = new ChromeDriver();
-		//site.get("http://yankton.com.br/#");
 		site.get("https://www.google.com/");
 		site.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
 		JavascriptExecutor js = (JavascriptExecutor) site;
-		List<WebElement> Elements = ElementsPage.getElements(site, js);
+		List<WebElement> Element = ElementsPage.getElements(site, js);
 		WebElement atual = null;
 		Actions acao = new Actions(site);
 		
 		PreventDefaultClick.PreventDefault(js); 
-		for(int i = 0; i < Elements.size(); i++) {
+		for(int i = 0; i < Element.size(); i++) {
 			atual = null;			
-			atual = Elements.get(i);
+			atual = Element.get(i);
 			if(atual.getSize().height > 0 && atual.getSize().width > 0) {
 				MutationScript.ScriptPorElemento(js, atual);
 				acao.moveToElement(atual).pause(300);
@@ -42,7 +41,5 @@ public class projeto {
 		//site.close();
 		//1- Aceitando as ações em div(getElements), foi notado uma maior coleita de dados mas ao mesmo tempo dados repetidos.
 		//2- If verificando height e width melhorou o desempenho de velocidade.
-		//3- Pensamento: pegar meu dados/screenshots no ambiente javaScript e depois mandar apenas para o Java (não sei relação a imagem);
-		
 	}
 }
